afee19d3f6805af40eca9810b206333af975d7ef
Uma Shankar Bharawa <ubharawa@gmail.com>
2024-05-15 15:20:17 +0530

    Final changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import Header from './components/Header';
 import Nav from './components/Nav';
 import Main from './components/Main';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Menu from './components/Menu';
 import Footer from './components/Footer';
+import './App.css'
 
 function App() {
   return (
-    <>
-      <Header/>
-      <Nav/>
-      <Main/>
-      <Footer/>
-    </>
+    <Router>
+      <>
+        <Nav />
+        <Main />
+        <Menu />
+        <Footer/>
+      </>
+    </Router>
   );
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,43 @@
 import React from 'react';
+import logo from '../images/Logo.svg'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer>
+            <section>
+                <div className='company-info'>
+                    <img src={logo} alt='' />
+                    <p>We are a family owned Mediterraneran restaurant, focused on traditional recipes served with a modern twist.</p>
+                </div>
+                <div>
+                    <h3>Important Links</h3>
+                    <ul>
+                        <li><a href='/'>Home</a></li>
+                        <li><a href='/'>About</a></li>
+                        <li><a href='/'>Menu</a></li>
+                        <li><a href='/'>Reservation</a></li>
+                        <li><a href='/'>Order Online</a></li>
+                        <li><a href='/'>Login</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>Contact</h3>
+                    <ul>
+                        <li>Address: <br/> Towncity, USA</li>
+                        <li>Phone: <br/> +12-3456 789 012</li>
+                        <li>Email: <br/> little@lemon.com</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>Social Media Links</h3>
+                    <ul>
+                        <li><a href='/'>Facebook</a></li>
+                        <li><a href='/'>X</a></li>
+                        <li><a href='/'>Instagram</a></li>
+                    </ul>
+                </div>
+            </section>
+        </footer>
     )
 }
 
